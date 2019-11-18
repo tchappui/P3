864ae41728dce6a94b990a4cc6b6754bbda04526
Vinke <56028074+Vinke-dev@users.noreply.github.com>
2019-11-18 10:10:59 +0100

    Create game.py

---
+++ game.py
@@ -0,0 +1,17 @@
+from labyrinth import Labyrinth
+from hero import Hero
+from items import Items
+from position import Position
+
+class Game:
+
+    def __init__(self):
+        self.labyrinth = Labyrinth()
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    pass
+
