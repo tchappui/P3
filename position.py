a885e7575b10ffec1b78ad6f0ce8a9d8c7265899
Vinke <56028074+Vinke-dev@users.noreply.github.com>
2019-11-18 09:40:34 +0100

    Update position.py

--- position.py
+++ position.py
@@ -4,17 +4,21 @@ class Position:
         self.x = x
         self.y = y
 
-    #Deplacement    
+    # Deplacement
     def up(self):
         return Position(self.y + 1, self.x)
 
     def down(self):
         return Position(self.y - 1, self.x)
-    
+
     def left(self):
         return Position(self.x - 1, self.y)
 
     def right(self):
         return Position(self.x + 1, self.y)
 
-    
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
