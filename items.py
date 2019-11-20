7b9038b0ad8cde5ea5c9b4374b994ea6954e968b
Thierry Chappuis <tchappui@users.noreply.github.com>
2019-11-20 16:36:23 +0100

    Update items.py

--- items.py
+++ items.py
@@ -1,19 +1,33 @@
 from random import sample #import random.sample for init & add item in labyrinth
 
+class Item:
+    
+    def __init__(self, name, position):
+        self.name = name
+        self.position
+        
+Item("needle", Position(10, 5))
+("needle", Position(10, 5))
+
 class Items:
     
     def __init__(self, labyrinth):
         self.labyrinth = labyrinth
-        self.item = []
+        self.items = {}
         self.random = None
         self.position = None
 
-    def initialize_random_positions(self):
-        self.random = iter(
-            random.sample(self.labyrinth.paths, k=len(self.labyrinth.paths))
-        )
-
-    def add_item_randomly(self, item):
-        self.item = random.sample(
+    def add_items_randomly(self):
+        positions = sample(
             set(self.labyrinth.paths)-{self.labyrinth.start, self.labyrinth.end}, 3)
-        return self.item
+        needle_pos, tube_pos, ether_pos = positions
+        self.items[needle_pos] = "needle"
+        self.items[tube_pos] = "tube"
+        self.items[ether_pos] = "ether"
+        
+    def catch_item(self, position):
+        if position in self.items:
+            item = self.items[position]
+            del elf.items[position]
+            return item
+  
